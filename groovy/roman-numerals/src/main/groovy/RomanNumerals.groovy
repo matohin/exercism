81b69858c97b752a854a8dfbe3e77445158e9b3c
Evgeniy Matohin <matohin@gmail.com>
2020-02-23 19:44:28 +0300

    roman numerals first working

--- groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
+++ groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
@@ -7,28 +7,34 @@ class RomanNumerals {
             def arabicValueToRepresent = delegate
 
             def romanNumerals = [ 1000:"M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 1:"I" ]
+            def numeralsBeginingWithFive = ["D", "L", "V"]
 
-            romanNumerals.eachWithIndex{numeralValue, numeralSign, i ->
+            romanNumerals.eachWithIndex {numeralValue, numeralSign, i ->
 
                 def intDivisionResult = arabicValueToRepresent.intdiv(numeralValue)
+                def remainder = arabicValueToRepresent % numeralValue
 
-                println intDivisionResult
-                println arabicValueToRepresent % numeralValue
-                println '================='
+                def leftSideNumeralIndex = numeralsBeginingWithFive.contains(numeralSign) ? (i + 1) : (i + 2)
 
-                if (numeralValue - arabicValueToRepresent == 1){
-                    romanRepresentation += romanNumerals.values()[i+2]
-                    romanRepresentation += numeralSign
+                def aaa = 0
+                if (leftSideNumeralIndex < romanNumerals.size()){
+                    aaa = (arabicValueToRepresent + (romanNumerals.keySet()[leftSideNumeralIndex])).intdiv(numeralValue)
                 }
 
-                else if (intDivisionResult <= 3){
+
+                if ((intDivisionResult <= 3) && (intDivisionResult > 0)) {
                     romanRepresentation += numeralSign * intDivisionResult
-                    arabicValueToRepresent = arabicValueToRepresent % numeralValue
+                    arabicValueToRepresent = remainder
                 }
 
+                else if (aaa == 1){
+                    romanRepresentation += romanNumerals.values()[leftSideNumeralIndex]
+                    romanRepresentation += numeralSign
 
+                    arabicValueToRepresent -= numeralValue
+                    arabicValueToRepresent += romanNumerals.keySet()[leftSideNumeralIndex]
+                }
             }
-
             return romanRepresentation
         }
     }
