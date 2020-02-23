c2e1d689068f2daa4aee4d4bc62d26281affd670
Evgeniy Matohin <matohin@gmail.com>
2020-02-23 20:41:47 +0300

    empty line removed

--- groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
+++ groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
@@ -3,7 +3,6 @@ class RomanNumerals {
         Integer.metaClass.getRoman = { ->
 
             def romanRepresentation = ''
-
             def arabicValueToRepresent = delegate
 
             def romanNumerals = [ 1000:"M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 1:"I" ]
